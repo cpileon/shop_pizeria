634e3ed17a4f73b15e38ed5cb4b2b392f37cfb84
CPinoLeon <cpino012@gmail.com>
2023-05-28 17:19:01 -0400

    Carousel Fixed

--- src/assets/components/Carrousel.jsx
+++ src/assets/components/Carrousel.jsx
@@ -11,44 +11,23 @@ const Carrusel = () => {
     };
     return(
         <>
-            <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel activeIndex={index} onSelect={handleSelect} className='shadow'>
                 <Carousel.Item>
                     <img
-                    className="d-block w-50"
-                    src="../../src/imgs/pizza_placeholder.png"
+                    className="d-block w-100"
+                    src="../../src/imgs/pizza_c_1.png"
                     alt="First slide"
                     />
-                    <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
                 <Carousel.Item>
                     <img
-                    className="d-block w-50"
-                    src="../../src/imgs/pizza_placeholder.png"
+                    className="d-block w-100"
+                    src="../../src/imgs/pizza_c_2.png"
                     alt="Second slide"
                     />
-
-                    <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
-                <Carousel.Item>
-                    <img
-                    className="d-block w-50"
-                    src="../../src/imgs/pizza_placeholder.png"
-                    alt="Third slide"
-                    />
 
-                    <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                    </p>
-                    </Carousel.Caption>
-                </Carousel.Item>
             </Carousel>
         </>
     );
