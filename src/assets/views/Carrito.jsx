bbbd727ad6bb7355bfbfe65b9526bacfe9eb348d
CPinoLeon <cpino012@gmail.com>
2023-05-28 13:03:29 -0400

    Minor changes to shopping cart

--- src/assets/views/Carrito.jsx
+++ src/assets/views/Carrito.jsx
@@ -7,19 +7,34 @@ import Container from 'react-bootstrap/Container';
 
 
 const Carrito = () =>{
-    const { datapizza, setDatapizza, pedido, setPedido, generarPedido } = useContext(MyContext)
+    const { datapizza, setDatapizza, pedido, setPedido, generarPedido, valorTotal, setValorTotal } = useContext(MyContext)
     return(
         <>
         <Container >
             <h3 className="mt-3">¡Chequea tu pedido!</h3>
-            {pedido.map((pizza, i)=>(
-                <div key={pizza.id}>
-                    {i+1} Pizza {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)} Cantidad: {pizza.cantidad}
+            <div>
+                {pedido.map((pizza, i)=>(
+                    <div className="row" key={pizza.id}>
+                        <div className="col">
+                            {i+1} 
+                        </div>
+                        <div className="col">
+                            <img src={pizza.img} width={50} />
+                        </div>
+                        <div className="col">
+                            Pizza {pizza.name.charAt(0).toUpperCase() + pizza.name.slice(1)}
+                        </div>
+                        <div className="col">
+                            <strong>Precio:</strong> ${pizza.price.toLocaleString('en-US')}
+                        </div>
+                        Cantidad: {pizza.cantidad}
                     </div>
-            ))}
-            <div className="d-flex justify-content-between">
-                <Link to="/"><Button className="btn btn-secondary">Volver</Button></Link>
-                <Button>Confirmar pedido</Button>
+                ))}
+                <h4>Total Pedido: ${valorTotal.toLocaleString('en-US')}</h4>
+                <div className="d-flex justify-content-between">
+                    <Link to="/"><Button className="btn btn-secondary">Volver</Button></Link>
+                    <Button>Confirmar pedido</Button>
+                </div>
             </div>
         </Container>
 
